149f7871b00dfc9747a5df11880d770eede026d9
Marwan Omran <Marwanmimo98@gmail.com>
2025-07-05 17:32:35 +0300

    s

--- js/Scripts.js
+++ js/Scripts.js
@@ -9,7 +9,7 @@ document.getElementById('submitButton').addEventListener('click', function () {
     document.getElementById('chatBox').appendChild(userBubble);
 
     // Send request to AI service
-    fetch(`http://3dsoftwareemergence.zapto.org:8000/query`, {
+    fetch(`http://3dsoftwareemergence.zapto.org:11434/api/generate`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ query })
